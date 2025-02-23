fe7f76e62cb739bc4a3faa305c31d7803dc82aa4
jefe-de-jefes <luissegovia141005@gmail.com>
2025-02-23 10:32:48 -0600

    Funciones de validacion

--- banco.cpp
+++ banco.cpp
@@ -1,8 +1,41 @@
 #include<iostream>
+#include <limits>
 
 using namespace std;
 
-int main(){
+template <typename T>
+T leerValor(int menor, int mayor){
+    T valor;
+    while (true)
+    {
+        try
+        {
+            cin >> valor;
+            if (cin.fail())
+            {
+                throw exception();
+            }
+            
+            if (valor < menor || valor > mayor)
+            {
+                cout << "\n**Introduzca un numero dentro de rango**" << endl;
+            }else{
+                return valor;
+            }
+        }
+        catch(const exception& e)
+        {
+            cout << "Numero no valido" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
 
-    
+}
+
+int main(){
+    int val;
+    cout << "Introduzca la matricula: ";
+    val = leerValor<int>(2, 4);
+   
 }
